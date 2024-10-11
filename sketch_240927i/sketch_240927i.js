381edf82dc0ce9f64b890ba0b77438a0eec27aee
lemuruz <nakabodeepatiparn@gmail.com>
2024-10-11 14:08:55 +0700

    fix critical mode
    earier this mode has a bug that the player cant press the 21th to 40th grid
    i add the number up to 40 number insted of 20

--- sketch_240927i/sketch_240927i.js
+++ sketch_240927i/sketch_240927i.js
@@ -2,8 +2,8 @@
 //add multiplayer
 //hint tell player the distanc between the pair 
 //timer
-let gameMode = "hard"; // You can change the game mode here
-let matchMode = "2match"
+let gameMode = "critical"; // You can change the game mode here
+let matchMode = "4match"
 let row_ = 0;
 let colum_ = 6;
 let clicked1 = [];
@@ -30,9 +30,9 @@ function setup() {
     background('white');
     let numbers = null;
     if (matchMode == "2match"){
-      numbers = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10,10];
+      numbers = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10,10,11,11,12,12,13,13,14,14,15,15,16,16,17,17,18,18,19,19,20,20];
     }else if(matchMode == "4match"){
-      numbers = [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5];
+      numbers = [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10];
     }
     
     
